398d7877a595baec5ba4f49e6d865a02dc4c75d4
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-01-31 08:11:33 -0800

    commit tree ab03f3fcedd3cd95310157c275900c0cbaefe3a3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+arr.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(arr)
+    i=0
+    new_arr=[]
+    while i < arr.length
+         new_arr << "Hello, #{arr[i]}! You'll be assigned to room #{(i + 1)}!"
+    i += 1
+    end
+    return new_arr
+end
+
+def printer(arr)
+    new_arr= assign_rooms(arr)
+    new_arr.each{|it| return it}
+end
